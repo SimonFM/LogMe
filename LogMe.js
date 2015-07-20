9b9744db443e373dcd7c08b4e5cf1505b07b80ae
Simon <markhams@tcd.ie>
2015-07-20 23:07:53 +0100

    Major Update
    Fixed the time, but still need to figure out how to format it with
    html.... I hate html...

--- LogMe.js
+++ LogMe.js
@@ -1,11 +1,36 @@
+var timeIterator;
+/*
+  The following code pushes the time spent on reddit in to the chrome storage
+  API.
+  if the data comes back as null or undefined then the extension starts off at 0.
+*/
 chrome.storage.local.get(['redditTime','timeR'], function(data2) {
-  console.log("Time Spent on reddit :", data2.timeR);
-  document.write("reddit: ",data2.timeR," seconds\n\n");
+  //console.log("Time Spent on reddit :", data2.timeR);
+  if(typeof data2.timeR == "undefined" || data2.timeR == null) timeIterator = 0;
+  else timeIterator = data2.timeR;
+  document.write("reddit -- ", secondsToString(timeIterator));
   
 });
-
+/*
+  The following code pushes the time spent on facebook in to the chrome storage
+  API.
+  if the data comes back as null or undefined then the extension starts off at 0.
+*/
 chrome.storage.local.get(['fbTime','timeF'], function(data1) {
-  console.log("Time Spent on FB :", data1.timeF);
-  document.write("facebook: ",data1.timeF," seconds");
+  //console.log("Time Spent on FB :", data1.timeF);
+  if(typeof data1.timeF == "undefined" || data1.timeF == null) timeIterator = 0;
+  else timeIterator = data1.timeF;
+  document.write("facebook -- ",secondsToString(timeIterator));
 });
-
+/*
+  Found this function on stackoverflow
+*/
+function secondsToString(seconds)
+{
+  var numyears = Math.floor(seconds / 31536000);
+  var numdays = Math.floor((seconds % 31536000) / 86400); 
+  var numhours = Math.floor(((seconds % 31536000) % 86400) / 3600);
+  var numminutes = Math.floor((((seconds % 31536000) % 86400) % 3600) / 60);
+  var numseconds = (((seconds % 31536000) % 86400) % 3600) % 60;
+  return numyears + " years " +  numdays + " days " + numhours + " hours " + numminutes + " minutes " + numseconds + " seconds";
+}
--- time.js
+++ time.js
@@ -4,51 +4,53 @@ var url = window.location.href;
 
 if(url.includes("reddit.com")){
   /*
-    
+    Get the time spent on reddit and set it to the the temp to be 
+    manipulated.
   */
   chrome.storage.local.get(['redditTime','timeR'], function(data) {
-    console.log("Time Spent on reddit :", data.timeR);
+    //console.log("Time Spent on reddit :", data.timeR);
     
-    if(typeof data.timeR == 'undefined') timeIterator = 0;
+    if(typeof data.timeR == "undefined" || typeof data.timeR == null) 
+      timeIterator = 0;
     else timeIterator = data.timeR;
   });
   
   /*
-    
+    This function acts as a timer and is activated every second
   */
   function myTimer2(){
     timeIterator = timeIterator + 1;
-    console.log("reddit time:",timeIterator,"seconds");
+    //console.log("reddit time:",timeIterator,"seconds");
     chrome.storage.local.set({redditTime:'reddit',timeR:timeIterator}, function() {});
   }
 
   var time = setInterval(function(){myTimer2()},1000);
 
 }
+/*
+  Otherwise we have found facebook and do the same as above.
+*/
 else if(url.includes("facebook.com")){
   /*
-    
+    Get the time spent on facebook and set it to the the temp to be 
+    manipulated.
   */
   chrome.storage.local.get(['fbTime','timeF'], function(data) {
-  console.log("Time Spent on facebook :", data.timeF);
+  //console.log("Time Spent on facebook :", data.timeF);
   
-   if(typeof data.timeF == 'undefined') timeIterator = 0;
+   if(typeof data.timeF == "undefined" || typeof data.timeF == null) 
+    timeIterator = 0;
    else timeIterator = data.timeF;
 });
 
-  /*
-    
+   /*
+    This function acts as a timer and is activated every second
   */
   function myTimer1(){
     timeIterator = timeIterator + 1;
-    console.log("facebook time:",timeIterator,"seconds");
+    //console.log("facebook time:",timeIterator,"seconds");
     chrome.storage.local.set({redditTime:'reddit',timeF:timeIterator}, function() {});
   }
 
   var time = setInterval(function(){myTimer1()},1000);
 }
-
-
-
-
-
